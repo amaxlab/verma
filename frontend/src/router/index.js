eab452c3956ae2a740e720c74fdb0cbfed6d1823
Egor Zyuskin <ezyuskin@codenetix.com>
2018-10-05 17:38:45 +0600

    feat: add vuejs

---
+++ frontend/src/router/index.js
@@ -0,0 +1,61 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import ScreenLogin from '@/screens/Login'
+
+Vue.use(Router)
+
+let router = new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: ScreenLogin,
+            meta: {
+                guest: true
+            }
+        }
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    if(to.matched.some(record => record.meta.requiresAuth)) {
+        if (localStorage.getItem('jwt') == null) {
+            next({
+                path: '/login',
+                params: { nextUrl: to.fullPath }
+            })
+        } else {
+            let user = JSON.parse(localStorage.getItem('user'))
+            if(to.matched.some(record => record.meta.is_admin)) {
+                if(user.is_admin == 1){
+                    next()
+                }
+                else{
+                    next({ name: 'userboard'})
+                }
+            }else {
+                next()
+            }
+        }
+    } else if(to.matched.some(record => record.meta.guest)) {
+        if(localStorage.getItem('jwt') == null){
+            next()
+        }
+        else{
+            next({ name: 'userboard'})
+        }
+    }else {
+        next()
+    }
+})
+
+export default router
