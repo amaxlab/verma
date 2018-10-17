c577eb19e47046ee8b57d460d006429aebf29842
Egor Zyuskin <ezyuskin@codenetix.com>
2018-10-17 11:16:49 +0600

    feat: add projects grid

---
+++ frontend/src/components/project/Grid.vue
@@ -0,0 +1,23 @@
+<template>
+    <DataGrid
+            :fields="[
+            {key:'id', label: 'ID'},
+            {key:'name', label: 'Name'},
+            {key:'enabled', label: 'Enabled'},
+            {key:'updatedAt', label: 'Updated At'}
+        ]"
+            ref="grid"
+            url="/api/projects"
+            defaultSortBy="name"
+            :defaultSortDesc="true"
+    ></DataGrid>
+</template>
+<script>
+    import DataGrid from '../DataGrid';
+
+    export default {
+        components: {
+            DataGrid
+        }
+    }
+</script>
--- frontend/src/screens/Projects.vue
+++ frontend/src/screens/Projects.vue
@@ -2,13 +2,15 @@
     <Page>
         <template slot="content">
             <h1>Projects</h1>
+            <Grid></Grid>
         </template>
     </Page>
 </template>
 <script>
     import Page from '../components/Page';
+    import Grid from "../components/project/Grid";
 
     export default {
-        components: {Page}
+        components: {Grid, Page}
     }
 </script>
