ae63e14eb82e3528519b30feb214c10a23bd64dc
Egor Zyuskin <ezyuskin@codenetix.com>
2018-10-19 10:59:43 +0600

    fix: set back btn margin

--- frontend/src/components/BackBtn.vue
+++ frontend/src/components/BackBtn.vue
@@ -1,7 +1,6 @@
 <template>
-    <b-button variant="link" @click="goBack"><i class="fa fa-chevron-left"></i></b-button>
+    <b-button class="btn-back" variant="link" @click="goBack"><i class="fa fa-chevron-left"></i></b-button>
 </template>
-
 <script>
     export default {
         methods: {
@@ -11,4 +10,8 @@
         }
     }
 </script>
-
+<style>
+    .btn-back {
+        margin-left: -37px;
+    }
+</style>
